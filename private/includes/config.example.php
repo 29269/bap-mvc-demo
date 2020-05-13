467d12c8b3fb981aa7739a8df56399f00d357558
Duneya <29269@ma-web.nl>
2020-05-13 12:22:33 +0200

    13/05

--- private/includes/config.example.php
+++ private/includes/config.example.php
@@ -6,11 +6,11 @@
 $config = [
 	'DB'       => [
 		'HOSTNAME' => '127.0.0.1',
-		'DATABASE' => '',
+		'DATABASE' => 'covid-19',
 		'USER'     => 'root',
 		'PASSWORD' => ''
 	],
-	'BASE_URL' => '',  // Zet hier het pad naar de public map in, vanaf http://localhost, anders werken je routes niet!
+	'BASE_URL' => 'bap-mvc-demo/',  // Zet hier het pad naar de public map in, vanaf http://localhost, anders werken je routes niet!
 	'ROOT'     => dirname( dirname( __DIR__ ) ),
 	'PRIVATE'  => dirname( __DIR__ ),
 	'WEBROOT'  => dirname( dirname( __DIR__ ) ) . '/public'
--- private/routes.php
+++ private/routes.php
@@ -11,7 +11,7 @@ SimpleRouter::group( [ 'prefix' => site_url() ], function () {
 	// START: Zet hier al eigen routes
 	// Lees de docs, daar zie je hoe je routes kunt maken: https://github.com/skipperbent/simple-php-router#routes
 
-	SimpleRouter::get( '/', 'WebsiteController@home' )->name( 'home' );
+	SimpleRouter::get( '/bap-mvc-demo', 'WebsiteController@home' )->name( 'home' );
 	// STOP: Tot hier al je eigen URL's zetten
 
 	SimpleRouter::get( '/not-found', function () {
